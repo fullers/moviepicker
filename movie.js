0bb16ccd5ff1e042a7ab4ab1584c9d455461531a
Cherish Khang <cherishkhang@gmail.com>
2016-09-08 19:01:50 -0400

    Update movie.js

--- movie.js
+++ movie.js
@@ -12,32 +12,34 @@ $('#search').on('submit', function(event) {
     $('#movieTitle').val('');
 	var queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
 	var imdbid;
+	var omdbResponse;
 
+	//Make Ajax call to OMDB
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-	     console.log(response);
 	     
-	     imdbid = response.imdbID;
-	     console.log(imdbid);
+		imdbid = response.imdbID;
+	    omdbResponse = response;
 
-	     if (movies.movie1 == '') {
-		     movies.movie1 = response;
-		     console.log('Movie 1 added');
-		    $('#movieTitle').attr('placeholder','Search for second movie');
-	 	}
-	 	else if (movies.movie2 == '') {
-		     movies.movie2 = response;
-		     console.log('Movie 2 added');
-	 	}
-
+	     //Make second Ajax call to IMDB API
 	    $.ajax({
-	        url: "http://imdb.wemakesites.net/api/" + imdbid,
-	        crossDomain: true,
-	        dataType: "jsonp",
-	        success: function(data) {
-	            window.console.log(data);
+        url: "http://imdb.wemakesites.net/api/" + imdbid,
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function(data) {
+            window.console.log(data);
 
-			    $('#movie1').append('Title: ' + movies.movie.Title);
-	        }
+		    if (movies.movie1 == '') {
+			     movies.movie1 = omdbResponse;
+			     console.log('Movie 1 added');
+			    $('#movieTitle').attr('placeholder','Search for second movie');
+			    $('#movie1').append('Title: ' + movies.movie1.Title);
+		 	}
+		 	else if (movies.movie2 == '') {
+			     movies.movie2 = omdbResponse;
+			     console.log('Movie 2 added');
+			    $('#movie2').append('Title: ' + movies.movie2.Title);
+		 	}
+        }
 	    });
 	}); 
 
