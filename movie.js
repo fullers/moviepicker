93bf41d964c9b25dab48cf8a58398d4ed3bc7aa9
Cherish Khang <cherishkhang@gmail.com>
2016-09-08 18:02:34 -0400

    Update movie.js and add movie 1

--- movie.js
+++ movie.js
@@ -1,7 +1,13 @@
+var movies = {
+	movie1: '',
+	movie2: ''
+}
+
 $('#search').on('submit', function(event) {
 	event.preventDefault();
 
 	var title = $('#movieTitle').val().trim();
+    $('#movieTitle').val('');
 	var queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
 	var imdbid;
 
@@ -11,6 +17,18 @@ $('#search').on('submit', function(event) {
 	     imdbid = response.imdbID;
 	     console.log(imdbid);
 
+	     if (movies.movie1 == '') {
+		     movies.movie1 = response;
+		     console.log('Movie 1 added');
+		     console.log(movies.movie1);
+		    $('#movieTitle').attr('placeholder','Search for second movie');
+	 	}
+	 	else if (movies.movie2 == '') {
+		     movies.movie2 = response;
+		     console.log('Movie 2 added');
+		     console.log(movies.movie2);
+	 	}
+
 	    $.ajax({
         url: "http://imdb.wemakesites.net/api/" + imdbid,
         crossDomain: true,
@@ -19,18 +37,16 @@ $('#search').on('submit', function(event) {
             window.console.log(data);
         }
 	    });
-
-
 	}); 
 
-	
+});
 
-	// var title2 = 'Aladdin';
-	// var queryURL2 = "http://www.omdbapi.com/?t=" + title2 + "&y=&plot=short&r=json";
+//Show 'loading' message when Ajax call is being run
+$( document ).ajaxStart(function() {
+    $( "#loading" ).show();
+});
 
-	// $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-	//      console.log(response);
-
-	//      console.log(response.Title)
-	// }); 
+//Hide 'loading' message when Ajax call is finished running
+$( document ).ajaxStop(function() {
+    $( "#loading" ).hide();
 });
