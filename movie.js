f427be081ee6bff4ae83caac14399252f7eae0b3
Cherish Khang <cherishkhang@gmail.com>
2016-09-10 00:13:08 -0400

    Move removal buttons in DOM, fade in/out movie info

--- movie.js
+++ movie.js
@@ -35,29 +35,29 @@ $('#search').on('submit', function(event) {
 		success: function(response) {
 			console.log('IMDB response added');
 
-			if (response.status == 'success') {
-				var btnClose = $('<i>');
-				btnClose.attr('class', 'fa fa-times');
-				btnClose.attr('area-hidden', 'true');
-				btnClose.attr('id', 'remove-movie1');
-
-				var btnClose2 = $('<i>');
-				btnClose2.attr('class', 'fa fa-times');
-				btnClose2.attr('area-hidden', 'true');
-				btnClose2.attr('id', 'remove-movie2');				
+			if (response.status == 'success') {	
 
 				if (movies.movie1 == '') {
 					movies.movie1 = omdbResponse;
-					$('#movie1').empty();					
+
+					//Empty movie1 div
+					$('#movie1').empty();
+
+					//Hide movie1 div
+					$('#movie1').hide();
+
+					var btnClose = $('<i>');
+					btnClose.attr('class', 'fa fa-times');
+					btnClose.attr('area-hidden', 'true');
+					btnClose.attr('id', 'remove-movie1');
+					$('#movie1').append(btnClose);
 					
 					var poster = $('<img>')
 					poster.attr('src',omdbResponse.Poster);
 					poster.attr('class','movie-poster');
 					poster.attr('id','movie1-poster');					
 					$('#movie1').append(poster);
-					$('#movie1').append(btnClose);
 					$('#movie1').append('<h2>' + movies.movie1.Title + '</h2>');
-					$( "#remove-movie1" ).show();
 
 					var table = $('<table>');
 					table.attr('class','u-full-width');
@@ -70,17 +70,30 @@ $('#search').on('submit', function(event) {
 					$('table#movie1-table').append('<tr><td>Metascore</td><td>' + movies.movie1.Metascore + '</td></tr>');
 					$('table#movie1-table').append('<tr><td>IMDb Rating</td><td>' + movies.movie1.imdbRating + '</td></tr>');
 					$('table#movie1-table').append('<tr><td>Plot</td><td>' + movies.movie1.Plot + '</td></tr>');
+
+					//Fade in movie1 div
+					$('#movie1').fadeIn();
 				}
 				else if (movies.movie2 == '') {
 					movies.movie2 = omdbResponse;
+
+					//Empty movie2 div
 					$('#movie2').empty();
+
+					//Hide movie2 div
+					$('#movie2').hide();
+
+					var btnClose = $('<i>');
+					btnClose.attr('class', 'fa fa-times');
+					btnClose.attr('area-hidden', 'true');
+					btnClose.attr('id', 'remove-movie2');
+					$('#movie2').append(btnClose);
 
 					var poster = $('<img>')
 					poster.attr('src',omdbResponse.Poster);
 					poster.attr('class','movie-poster');
 					poster.attr('id','movie2-poster');
 					$('#movie2').append(poster);
-					$('#movie2').append(btnClose2);
 					$('#movie2').append('<h2>' + movies.movie2.Title + '</h2>');
 					$( "#remove-movie2" ).show();
 
@@ -95,6 +108,9 @@ $('#search').on('submit', function(event) {
 					$('table#movie2-table').append('<tr><td>Metascore</td><td>' + movies.movie2.Metascore + '</td></tr>');
 					$('table#movie2-table').append('<tr><td>IMDb Rating</td><td>' + movies.movie2.imdbRating + '</td></tr>');
 					$('table#movie2-table').append('<tr><td>Plot</td><td>' + movies.movie2.Plot + '</td></tr>');
+
+					//Fade in movie2 div
+					$('#movie2').fadeIn();
 				}
 
 				//Check to see if there are two movies selected
@@ -111,22 +127,28 @@ $('#search').on('submit', function(event) {
 
 //Show 'loading' message when Ajax call is being run
 $( document ).ajaxStart(function() {
-	$( "#loading" ).show();
+	$('#loading').show();
 });
 
 //Hide 'loading' message when Ajax call is finished running
 $( document ).ajaxStop(function() {
-	$( "#loading" ).hide();
+	$('#loading').hide();
 });
 
 //On-click function to remove movie 1
 $(document).on('click', '#remove-movie1', function() {
-	$('#movie1').empty();
+	$('#movie1').fadeOut();
+	setTimeout(function () {
+		$('#movie1').empty();
+	},450);
 	movies.movie1 = '';
 });
 
 //On-click function to remove movie 2
 $(document).on('click', '#remove-movie2', function() {
-	$('#movie2').empty();
+	$('#movie2').fadeOut();
+	setTimeout(function() {
+		$('#movie2').empty();
+	},450);
 	movies.movie2 = '';
 });
