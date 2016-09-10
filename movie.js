14cfe8a37dd7bb03391d1743c4cb20af4cf1fe13
Cherish Khang <cherishkhang@gmail.com>
2016-09-10 01:22:18 -0400

    Add minor features to movie searches/displays

--- movie.js
+++ movie.js
@@ -16,113 +16,163 @@ var omdbResponse;
 $('#search').on('submit', function(event) {
 	event.preventDefault();
 
-	var title = $('#movieTitle').val().trim();
-	$('#movieTitle').val('');
-	var queryURL = "http://www.omdbapi.com/?t=" + title + "&plot=short&r=json";
-	var imdbid;
+	//If search field isn't empty
+	if ($('#movieTitle').val() !== '') {
 
-	//Make Ajax call to OMDB
-	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-		imdbid = response.imdbID;
-		omdbResponse = response;
-		console.log('OMDB response added');
+		var title = $('#movieTitle').val().trim();
+		$('#movieTitle').val('');
+		var queryURL = "http://www.omdbapi.com/?t=" + title + "&plot=short&r=json";
+		var imdbid;
 
-		//Make second Ajax call to IMDB API
-		$.ajax({
-		url: "http://imdb.wemakesites.net/api/" + imdbid,
-		crossDomain: true,
-		dataType: "jsonp",
-		success: function(response) {
-			console.log('IMDB response added');
+		//Make Ajax call to OMDB
+		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			imdbid = response.imdbID;
+			omdbResponse = response;
+			console.log('OMDB response added');
 
-			if (response.status == 'success') {	
+			//Make second Ajax call to IMDB API
+			$.ajax({
+			url: "http://imdb.wemakesites.net/api/" + imdbid,
+			crossDomain: true,
+			dataType: "jsonp",
+			success: function(response) {
+				console.log('IMDB response added');
 
-				if (movies.movie1 == '') {
-					movies.movie1 = omdbResponse;
+				if (response.status == 'success') {	
 
-					//Hide movie1 div
-					$('#movie1').hide();
+					if (movies.movie1 == '') {
+						movies.movie1 = omdbResponse;
 
-					//Add removal button
-					var btnClose = $('<i>');
-					btnClose.attr('class', 'fa fa-times removal-button');
-					btnClose.attr('area-hidden', 'true');
-					btnClose.attr('id', 'remove-movie1');
-					btnClose.attr('title', 'Remove movie');
-					$('#movie1').append(btnClose);
+						//Hide movie1 div
+						$('#movie1').hide();
 
-					//Add movie poster
-					var poster = $('<img>')
-					poster.attr('src',omdbResponse.Poster);
-					poster.attr('class','movie-poster');
-					poster.attr('id','movie1-poster');					
-					$('#movie1').append(poster);
-					$('#movie1').append('<h2>' + movies.movie1.Title + '</h2>');
+						//Add removal button
+						var btnClose = $('<i>');
+						btnClose.attr('class', 'fa fa-times removal-button');
+						btnClose.attr('area-hidden', 'true');
+						btnClose.attr('id', 'remove-movie1');
+						btnClose.attr('title', 'Remove movie');
+						$('#movie1').append(btnClose);
 
-					//Add table for the movie info
-					var table = $('<table>');
-					table.attr('class','u-full-width');
-					table.attr('id','movie1-table');
-					$('#movie1').append(table);
+						//Add movie poster
+						var poster = $('<img>')
+						if (omdbResponse.Poster == 'N/A') {
+							poster.attr('src','images/no-poster.jpg');
+						}
+						else {
+							poster.attr('src',omdbResponse.Poster);
+						}
+						poster.attr('class','movie-poster');
+						poster.attr('id','movie1-poster');
+						$('#movie1').append(poster);
 
-					//Add movie info to table
-					$('table#movie1-table').append('<tr><td>Year</td><td>' + movies.movie1.Year + '</td></tr>');
-					$('table#movie1-table').append('<tr><td>Rating</td><td>' + movies.movie1.Rated + '</td></tr>');
-					$('table#movie1-table').append('<tr><td>Actors</td><td>' + movies.movie1.Actors + '</td></tr>');
-					$('table#movie1-table').append('<tr><td>Metascore</td><td>' + movies.movie1.Metascore + '</td></tr>');
-					$('table#movie1-table').append('<tr><td>IMDb Rating</td><td>' + movies.movie1.imdbRating + '</td></tr>');
-					$('table#movie1-table').append('<tr><td>Plot</td><td>' + movies.movie1.Plot + '</td></tr>');
+						//Add movie title
+						$('#movie1').append('<h2>' + movies.movie1.Title + '</h2>');
 
-					//Fade in movie1 div
-					$('#movie1').fadeIn();
+						//Add table for the movie info
+						var table = $('<table>');
+						table.attr('class','u-full-width');
+						table.attr('id','movie1-table');
+						$('#movie1').append(table);
+
+						//Add movie info to table
+						if (movies.movie1.Year !== 'N/A') {
+							$('table#movie1-table').append('<tr><td>Year</td><td>' + movies.movie1.Year + '</td></tr>');
+						}
+						if (movies.movie1.Rated !== 'N/A') {
+							$('table#movie1-table').append('<tr><td>Rating</td><td>' + movies.movie1.Rated + '</td></tr>');
+						}
+						if (movies.movie1.Actors !== 'N/A') {
+							$('table#movie1-table').append('<tr><td>Actors</td><td>' + movies.movie1.Actors + '</td></tr>');
+						}
+						if (movies.movie1.Genre !== 'N/A') {
+							$('table#movie1-table').append('<tr><td>Genre</td><td>' + movies.movie1.Genre + '</td></tr>');
+						}
+						if (movies.movie1.Metascore !== 'N/A') {
+							$('table#movie1-table').append('<tr><td>Metascore</td><td>' + movies.movie1.Metascore + '</td></tr>');
+						}
+						if (movies.movie1.imdbRating !== 'N/A') {
+							$('table#movie1-table').append('<tr><td>IMDb Rating</td><td>' + movies.movie1.imdbRating + '</td></tr>');
+						}
+						if (movies.movie1.Plot !== 'N/A') {
+							$('table#movie1-table').append('<tr><td>Plot</td><td>' + movies.movie1.Plot + '</td></tr>');
+						}
+
+						//Fade in movie1 div
+						$('#movie1').fadeIn();
+					}
+					else if (movies.movie2 == '') {
+						movies.movie2 = omdbResponse;
+
+						//Hide movie2 div
+						$('#movie2').hide();
+
+						//Add removal button
+						var btnClose = $('<i>');
+						btnClose.attr('class', 'fa fa-times removal-button');
+						btnClose.attr('area-hidden', 'true');
+						btnClose.attr('id', 'remove-movie2');
+						btnClose.attr('title', 'Remove movie');
+						$('#movie2').append(btnClose);
+
+						//Add movie poster
+						var poster = $('<img>')
+						if (omdbResponse.Poster == 'N/A') {
+							poster.attr('src','images/no-poster.jpg');
+						}
+						else {
+							poster.attr('src',omdbResponse.Poster);
+						}
+						poster.attr('class','movie-poster');
+						poster.attr('id','movie2-poster');
+						$('#movie2').append(poster);
+
+						//Add movie title
+						$('#movie2').append('<h2>' + movies.movie2.Title + '</h2>');
+
+						//Add table for the movie info
+						var table = $('<table>');
+						table.attr('class','u-full-width');
+						table.attr('id','movie2-table');
+						$('#movie2').append(table);
+
+						//Add movie info to table
+						if (movies.movie2.Year !== 'N/A') {
+							$('table#movie2-table').append('<tr><td>Year</td><td>' + movies.movie2.Year + '</td></tr>');
+						}
+						if (movies.movie2.Rated !== 'N/A') {
+							$('table#movie2-table').append('<tr><td>Rating</td><td>' + movies.movie2.Rated + '</td></tr>');
+						}
+						if (movies.movie2.Actors !== 'N/A') {
+							$('table#movie2-table').append('<tr><td>Actors</td><td>' + movies.movie2.Actors + '</td></tr>');
+						}
+						if (movies.movie2.Genre !== 'N/A') {
+							$('table#movie2-table').append('<tr><td>Genre</td><td>' + movies.movie2.Genre + '</td></tr>');
+						}
+						if (movies.movie2.Metascore !== 'N/A') {
+							$('table#movie2-table').append('<tr><td>Metascore</td><td>' + movies.movie2.Metascore + '</td></tr>');
+						}
+						if (movies.movie2.imdbRating !== 'N/A') {
+							$('table#movie2-table').append('<tr><td>IMDb Rating</td><td>' + movies.movie2.imdbRating + '</td></tr>');
+						}
+						if (movies.movie2.Plot !== 'N/A') {
+							$('table#movie2-table').append('<tr><td>Plot</td><td>' + movies.movie2.Plot + '</td></tr>');
+						}
+
+						//Fade in movie2 div
+						$('#movie2').fadeIn();
+					}
+
+					//Check to see if there are two movies selected
+					if (movies.movie1 !== '' && movies.movie2 !== '') {
+						console.log('two movies!');
+					}
 				}
-				else if (movies.movie2 == '') {
-					movies.movie2 = omdbResponse;
-
-					//Hide movie2 div
-					$('#movie2').hide();
-
-					var btnClose = $('<i>');
-					btnClose.attr('class', 'fa fa-times removal-button');
-					btnClose.attr('area-hidden', 'true');
-					btnClose.attr('id', 'remove-movie2');
-					btnClose.attr('title', 'Remove movie');
-					$('#movie2').append(btnClose);
-
-					var poster = $('<img>')
-					poster.attr('src',omdbResponse.Poster);
-					poster.attr('class','movie-poster');
-					poster.attr('id','movie2-poster');
-					$('#movie2').append(poster);
-					$('#movie2').append('<h2>' + movies.movie2.Title + '</h2>');
-					$( "#remove-movie2" ).show();
-
-					var table = $('<table>');
-					table.attr('class','u-full-width');
-					table.attr('id','movie2-table');
-					$('#movie2').append(table);
-
-					$('table#movie2-table').append('<tr><td>Year</td><td>' + movies.movie2.Year + '</td></tr>');
-					$('table#movie2-table').append('<tr><td>Rating</td><td>' + movies.movie2.Rated + '</td></tr>');
-					$('table#movie2-table').append('<tr><td>Actors</td><td>' + movies.movie2.Actors + '</td></tr>');
-					$('table#movie2-table').append('<tr><td>Metascore</td><td>' + movies.movie2.Metascore + '</td></tr>');
-					$('table#movie2-table').append('<tr><td>IMDb Rating</td><td>' + movies.movie2.imdbRating + '</td></tr>');
-					$('table#movie2-table').append('<tr><td>Plot</td><td>' + movies.movie2.Plot + '</td></tr>');
-
-					//Fade in movie2 div
-					$('#movie2').fadeIn();
-				}
-
-				//Check to see if there are two movies selected
-				if (movies.movie1 !== '' && movies.movie2 !== '') {
-					console.log('two movies!');
-				}
+				else console.log('Response failed!');
 			}
-			else console.log('Response failed!');
-		}
-		});
-	}); 
-
+			});
+		}); 
+	}
 });
 
 //Show 'loading' message when Ajax call is being run
